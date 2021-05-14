886994107ea8ff7fe7f03937f54f317e30f1495d
willsnow06 <70275891+willsnow06@users.noreply.github.com>
2021-05-14 16:34:32 -0600

    Update wall.py

--- src/wall.py
+++ src/wall.py
@@ -1,1 +1,19 @@
+import pygame
 
+#this is litterally just a rectangle
+class Wall:
+     x = 0
+     y = 0
+     w = 0
+     h = 0
+     win = pygame.surface
+
+     def __init__(self, x, y, w, h, win):
+          self.x = x
+          self.y = y
+          self.w = w
+          self.h = h
+          self.win = win
+
+     def display(self):
+          pygame.draw.rect(self.win, (255, 0, 0), (self.x, self.y, self.w, self.h))
