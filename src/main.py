0ca4e72c926a37dfdd4043f9fdbc2d1f8bfee8e6
willsnow06 <70275891+willsnow06@users.noreply.github.com>
2021-05-19 07:52:48 -0600

    Update main.py

--- src/main.py
+++ src/main.py
@@ -12,18 +12,21 @@ pygame.init()
 
 win = pygame.display.set_mode((500, 500))
 
-pygame.display.set_caption('cheese nuts')
+pygame.display.set_caption('game')
 
 
 x = 225
 y = 225
 ex = 0
 ey = 250
-vel = 30
+vel = 25
 evel = 1
 
 width = 50
 height = 50
+
+levelx = 1
+levely = 1
 
 wall1 = Wall(0, 0, 100, 50, win)
 wall2 = Wall(0, 0, 50, 100, win)
@@ -39,6 +42,11 @@ wall8 = Wall(400, 450, 100, 50, win)
 
 walls = [wall1, wall2, wall3, wall4, wall5, wall6, wall7, wall8]
 
+image = pygame.image
+background = pygame.image.load(r'bricks.jpg')
+left = bool
+right = bool
+
 run = True
 while run:
     pygame.time.delay(100)
@@ -46,31 +54,66 @@ while run:
         if event.type == pygame.QUIT:
             run = False
 
+    win.blit(background, (0, 0))
+
     key = pygame.key.get_pressed()
+
+    if right == True:
+      left = False
+    if left == True:
+      right = False
 
     if key[pygame.K_LEFT] or key[pygame.K_a]:
         x -= vel
+        left = True
+        right = False
     if key[pygame.K_RIGHT] or key[pygame.K_d]:
         x += vel
+        right = True
+        left = False
     if key[pygame.K_UP] or key[pygame.K_w]:
         y -= vel
     if key[pygame.K_DOWN] or key[pygame.K_s]:
         y += vel
     
 
-
-    win.fill((0, 0, 0))
     dude = Character(x, y, width, height, win)
     bruh = Enemie(ex, ey, 30, 30, win)
+
+    if x <= 0:
+      levelx = levelx + 1
+      x = 499
+    
+    if x >= 500:
+      levelx = levelx - 1
+      x = 1
+
+    if y <= 0:
+      levely = levely + 1
+      y = 499
+
+    if y >= 500:
+      levely = levely - 1
+      y = 1
     
     if key[pygame.K_SPACE]:
       dude.attack = True
-      if ex >= x - width or ex <= x + width or ey >= y - height or ey <= y + height:
+      if ex >= x - width and ex <= x + width and ey >= y - height and ey <= y + height:
         evel = 0
         ex = -100
         ey = -100
+      vel = 0
+      if right:
+        image = pygame.image.load(r'attack.png')
+      if left:
+        image = pygame.image.load(r'attackInverse.png')
     else:
       dude.attack = False
+      vel = 25
+      if right:
+        image = pygame.image.load(r'sword_dude.png')
+      if left:
+        image = pygame.image.load(r'sword_dudeInverse.png')
 
     for Wall in walls:
       wall = Wall
@@ -86,8 +129,8 @@ while run:
       bruh.dRight(dude)
       bruh.dLeft(dude)
       bruh.dTop(dude)
-      bruh.dBottom(dude)
-      dude.display()
+      bruh.dBottom(dude) 
+      win.blit(image, (x - dude.w, y - dude.h))
       bruh.display()
       if dude.right:
         x  = x + vel
